1081ebaeb5014f95b91b53557cb680a5d6bb2382
bclow <bclow.tw@yahoo.com.tw>
2019-05-22 17:15:57 +0800

    add example of export sheet as tsv file

--- sendSheetsData.php
+++ sendSheetsData.php
@@ -4,6 +4,8 @@
 // 1. https://developers.google.com/sheets/api/quickstart/php#step_3_set_up_the_sample
 // 2. https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/append
 //
+// 3. if you want to download google sheet as tsv, we need to use drive api : 
+//    https://developers.google.com/drive/api/v3/manage-downloads
 
 
 
@@ -14,6 +16,16 @@ require_once __DIR__ . '/../../../composer/vendor/autoload.php';
 // https://developers.google.com/identity/protocols/OAuth2ServiceAccount#delegatingauthority
 // https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/append
 // https://stackoverflow.com/questions/46256676/google-sheets-api-v4-method-spreadsheets-values-append 
+//
+function downloadSheet($sheetID) {
+    $client         = getClientHeadless();
+    $service        = new Google_Service_Drive($client);
+    $response       = $service->files->export($sheetID, "text/tab-separated-values", [ "alt" => "media" ]);
+    $content = $response->getBody()->getContents();
+    
+    echo $content;
+}
+
 
 function sendData($spreadsheetId, $sheet, $value) {
     //  // The ID of the spreadsheet to update.
